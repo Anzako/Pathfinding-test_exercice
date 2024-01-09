ca78a8f3a06258350f29c241c4b5d10fea4b5289
Anzako <antektop@onet.eu>
2024-01-09 17:11:26 +0100

    Add GridNode and Node script
    Create GridNode by given gridWorldSize, Collision basen on Layermask - checking walkable node

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class Node
 {
-    
+    public bool isWalkable;
+    public Vector3 worldPosition;
+
+    public Node(bool _isWalkable, Vector3 _worldPosition)
+    {
+        isWalkable = _isWalkable;
+        worldPosition = _worldPosition;
+    }
 }
